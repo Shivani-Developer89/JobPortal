52b28cce2b38b1acb91489719507e1da7511fb20
Shivani-Developer89 <shivani25782@gmail.com>
2026-05-23 23:59:25 +0530

    write logic for methods

--- src/main/java/com/JobPortal/JPP/service/UserServiceImpl.java
+++ src/main/java/com/JobPortal/JPP/service/UserServiceImpl.java
@@ -3,18 +3,49 @@ package com.JobPortal.JPP.service;
 import com.JobPortal.JPP.dto.request.RegisterInputDTO;
 import com.JobPortal.JPP.dto.response.RegisterOutputDTO;
 import com.JobPortal.JPP.entity.User;
+import com.JobPortal.JPP.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public List<RegisterOutputDTO> getAllUsers() {
-        return List.of();
+        List<User> userList = userRepository.findAll();
+        List<RegisterOutputDTO> registerOutputDTOList = new ArrayList<>();
+        for(User user : userList){
+            RegisterOutputDTO registerOutputDTO = new RegisterOutputDTO();
+
+            registerOutputDTO.setId(user.getId());
+            registerOutputDTO.setName(user.getName());
+            registerOutputDTO.setEmail(user.getEmail());
+            registerOutputDTO.setPassword(user.getPassword());
+            registerOutputDTO.setRole(user.getRole());
+
+            registerOutputDTOList.add(registerOutputDTO);
+        }
+
+        return registerOutputDTOList;
     }
 
     @Override
     public RegisterOutputDTO getUserById(Long id) {
-        return null;
+        User user = userRepository.findById(id).orElse(null);
+        RegisterOutputDTO registerOutputDTO = new RegisterOutputDTO();
+        registerOutputDTO.setId(user.getId());
+        registerOutputDTO.setName(user.getName());
+        registerOutputDTO.setEmail(user.getEmail());
+        registerOutputDTO.setPassword(user.getPassword());
+        registerOutputDTO.setRole(user.getRole());
+
+        return registerOutputDTO;
     }
 
     @Override
@@ -22,12 +53,41 @@ public class UserServiceImpl implements UserService {
         User user = new User();
 
         user.setName(registerInputDTO.getName());
-        return null;
+        user.setEmail(registerInputDTO.getEmail());
+        user.setPassword(registerInputDTO.getPassword());
+        user.setRole(registerInputDTO.getRole());
+
+        user = userRepository.save(user);
+
+        RegisterOutputDTO registerOutputDTO = new RegisterOutputDTO();
+        registerOutputDTO.setId(user.getId());
+        registerOutputDTO.setName(user.getName());
+        registerOutputDTO.setEmail(user.getEmail());
+        registerOutputDTO.setPassword(user.getPassword());
+        registerOutputDTO.setRole(user.getRole());
+        return  registerOutputDTO;
     }
 
     @Override
     public RegisterOutputDTO updateUser(Long id, RegisterInputDTO registerInputDTO) {
-        return null;
+        User user = new User();
+
+        user.setId(id);
+        user.setName(registerInputDTO.getName());
+        user.setEmail(registerInputDTO.getEmail());
+        user.setPassword(registerInputDTO.getPassword());
+        user.setRole(registerInputDTO.getRole());
+
+        user = userRepository.save(user);
+
+        RegisterOutputDTO registerOutputDTO = new RegisterOutputDTO();
+        registerOutputDTO.setId(user.getId());
+        registerOutputDTO.setName(user.getName());
+        registerOutputDTO.setEmail(user.getEmail());
+        registerOutputDTO.setPassword(user.getPassword());
+        registerOutputDTO.setRole(user.getRole());
+        return  registerOutputDTO;
+
     }
 
     @Override
