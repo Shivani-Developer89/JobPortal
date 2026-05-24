af92c87cf727bc004973161959228f8fc673a634
Shivani-Developer89 <shivani25782@gmail.com>
2026-05-24 14:38:46 +0530

    Adds Validation in user entity

--- src/main/java/com/JobPortal/JPP/entity/User.java
+++ src/main/java/com/JobPortal/JPP/entity/User.java
@@ -2,6 +2,8 @@ package com.JobPortal.JPP.entity;
 
 import com.JobPortal.JPP.entity.enums.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -11,9 +13,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
     @Column(unique = true)
+    @Email
     private String email;
+    @NotBlank
     private  String password;
     @Enumerated(EnumType.STRING)
     private Role role;
