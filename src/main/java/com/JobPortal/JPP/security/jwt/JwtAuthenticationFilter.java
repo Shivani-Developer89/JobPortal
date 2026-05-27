22504598f453afae37189e994a7fb75182cd1c45
Shivani-Developer89 <shivani25782@gmail.com>
2026-05-27 23:17:14 +0530

    Adds method to check bearer token from the user

--- src/main/java/com/JobPortal/JPP/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/JobPortal/JPP/security/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,46 @@
 package com.JobPortal.JPP.security.jwt;
 
-public class JwtAuthenticationFilter {
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter
+        extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException, java.io.IOException {
+
+        String authHeader =
+                request.getHeader("Authorization");
+
+        if (authHeader == null
+                || !authHeader.startsWith("Bearer ")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token =
+                authHeader.substring(7);
+
+        String email =
+                jwtService.extractEmail(token);
+
+        System.out.println(email);
+
+        filterChain.doFilter(request, response);
+    }
 }
