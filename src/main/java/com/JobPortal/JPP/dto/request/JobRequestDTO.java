03085ec3e7768db26cffbc764e32d96cde6fc7a1
Shivani-Developer89 <shivani25782@gmail.com>
2026-05-29 23:30:31 +0530

    Adds DTO for Job

---
+++ src/main/java/com/JobPortal/JPP/dto/request/JobRequestDTO.java
@@ -0,0 +1,12 @@
+package com.JobPortal.JPP.dto.request;
+
+import java.time.LocalDate;
+
+public class JobRequestDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String location;
+    private Double Salary;
+    private LocalDate createdAt;
+}
---
+++ src/main/java/com/JobPortal/JPP/dto/response/JobResponseDTO.java
@@ -0,0 +1,13 @@
+package com.JobPortal.JPP.dto.response;
+
+import java.time.LocalDate;
+
+public class JobResponseDTO {
+
+    private String title;
+
+    private String description;
+    private String location;
+    private Double Salary;
+    private LocalDate createdAt;
+}
