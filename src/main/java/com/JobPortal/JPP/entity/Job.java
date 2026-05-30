76154e79aa05360a2ccdbfa3d7f4c8a3b1976a74
Shivani-Developer89 <shivani25782@gmail.com>
2026-05-30 21:57:55 +0530

    Adds Lombok

--- src/main/java/com/JobPortal/JPP/entity/Job.java
+++ src/main/java/com/JobPortal/JPP/entity/Job.java
@@ -4,9 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 import java.time.LocalDate;
-
+@Data
 public class Job {
 
     @Id
