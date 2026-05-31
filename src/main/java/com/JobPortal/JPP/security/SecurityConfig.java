002d61bb1535165ddfb880eabcf167f066ce7ca6
Shivani-Developer89 <shivani25782@gmail.com>
2026-05-31 22:45:23 +0530

    Change in permit all request

--- src/main/java/com/JobPortal/JPP/security/SecurityConfig.java
+++ src/main/java/com/JobPortal/JPP/security/SecurityConfig.java
@@ -23,11 +23,9 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/auth/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
+
+                        .authorizeHttpRequests(auth -> auth
+                                .anyRequest().permitAll()
                 )
                 .addFilterBefore(
                         jwtAuthenticationFilter,
