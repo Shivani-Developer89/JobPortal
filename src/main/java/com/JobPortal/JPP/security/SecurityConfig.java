4125b782aa87f343e75d684e78cc31db00b663ff
Shivani-Developer89 <shivani25782@gmail.com>
2026-06-22 22:04:56 +0530

    Allows permision

--- src/main/java/com/JobPortal/JPP/security/SecurityConfig.java
+++ src/main/java/com/JobPortal/JPP/security/SecurityConfig.java
@@ -22,10 +22,13 @@ public class SecurityConfig {
     ) throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
-
-                        .authorizeHttpRequests(auth -> auth
-                                .anyRequest().permitAll()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(
                         jwtAuthenticationFilter,
